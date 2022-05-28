e0569fd88eb8c0096c15f57ccd221c683a6b10c9
EricTGoto <erictomokigoto@gmail.com>
2022-05-28 10:13:51 -0700

    added functionality to edit note

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,14 +1,29 @@
 import React from 'react';
 
 function Overview({ tasks, deleteFunction }) {
-	const taskList = tasks.map((task) => {
-		return (
-  		<div key={task.taskID}>
-    		<p>{task.taskText}</p>
-    		<button id={task.taskID} onClick={deleteFunction}>Delete</button>
-    	</div>
-  	)}
-	);
+  function handleEdit(e) {
+    // if contenteditable is true then turn it off and vice versa
+    const { target } = e;
+    const pElement = target.previousSibling;
+    const isEditable = pElement.getAttribute('contenteditable'); // string
+    console.log(typeof isEditable);
+
+    if (isEditable === 'true') {
+      pElement.setAttribute('contenteditable', 'false');
+    } else {
+      pElement.setAttribute('contenteditable', 'true');
+    }
+  }
+
+  const taskList = tasks.map((task) => {
+    return (
+      <div key={task.taskID} className="task">
+        <p contentEditable="true">{task.taskText}</p>
+        <button type="button" onClick={handleEdit}>Edit</button>
+        <button type="button" id={task.taskID} onClick={deleteFunction}>Delete</button>
+      </div>
+    );
+  });
   return (
     <div>
       {taskList}

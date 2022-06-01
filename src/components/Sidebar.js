1dac1ab628224ad0d51c3ca20abee552e020f9e9
EricTGoto <erictomokigoto@gmail.com>
2022-05-31 20:51:21 -0700

    added styling to sidebar items

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,7 +3,9 @@ import React from 'react';
 function Sidebar({ show }) {
   return (
     <div className={show ? 'sidebar show' : 'sidebar hide'}>
-      Sidebar
+      <div className="sidebar-item">First Item</div>
+      <div className="sidebar-item">Second Item</div>
+      <div className="sidebar-item">Third Item</div>
     </div>
   );
 }

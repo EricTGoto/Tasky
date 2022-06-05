de4a7c364188940b7eae2f829f07149817718f76
EricTGoto <erictomokigoto@gmail.com>
2022-06-04 20:54:02 -0700

    fixed edit button

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,18 +3,18 @@ import trash from '../images/trash.svg';
 import edit from '../images/edit.svg';
 
 function Tasks({ tasks, deleteFunction, sidebarShowing }) {
-  function handleEdit(e) {
+  const [editable, setEditable] = React.useState('false');
+
+  function handleEdit() {
     // if contenteditable is true then turn it off and vice versa
-    const { target } = e;
-    const titleElement = target.parentElement.firstChild;
-
-    const isEditable = titleElement.getAttribute('contenteditable'); // string
-
-    if (isEditable === 'true') {
-      titleElement.setAttribute('contenteditable', 'false');
-    } else {
-      titleElement.setAttribute('contenteditable', 'true');
-    }
+    setEditable((prevEditable) => {
+      if (prevEditable === 'false') {
+        return 'true';
+      // eslint-disable-next-line no-else-return
+      } else {
+        return 'false';
+      }
+    });
   }
 
   const taskContainerSize = sidebarShowing ? '2' : '1 / 3';
@@ -24,18 +24,10 @@ function Tasks({ tasks, deleteFunction, sidebarShowing }) {
     taskContainer.style.gridColumn = taskContainerSize;
   }, [sidebarShowing]);
 
-  function handleClick(e) {
-    console.log(e);
-  }
-
-  function handleKeyPress(e) {
-    console.log(e);
-  }
-
   const taskList = tasks.map((task) => {
     return (
       <div key={task.taskID} className="task">
-        <p contentEditable="false" role="presentation" className="task-title" onClick={handleClick} onKeyDown={handleKeyPress}>{task.taskText}</p>
+        <p contentEditable={editable} role="presentation" className="task-title">{task.taskText}</p>
         <div className="task-right">
           <p>Date</p>
           <input type="image" src={edit} onClick={handleEdit} alt="edit" />

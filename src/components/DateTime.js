b98a95dd764d80b79936e5d8b18489d4fba4d5a4
EricTGoto <erictomokigoto@gmail.com>
2022-06-11 15:13:27 -0700

    refactored using dateutils

--- src/components/DateTime.js
+++ src/components/DateTime.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import { isToday, parse } from 'date-fns';
+import { isItToday } from '../utils/DateUtils';
 
 // called DateTime so it doesn't overlap with the built in Date
 // dates from the html date input are formatted 'yyyy-MM-dd'
 // want to display it like June 5, 2022, or just the time if the task is due today
 function DateTime({ date, time }) {
-  function isItToday() {
-    const parsedDate = parse(date, 'yyyy-MM-dd', new Date());
-    return isToday(parsedDate);
-  }
-
   return (
-    <div>{isItToday() ? time : date}</div>
+    <div>{isItToday(date) ? time : date}</div>
   );
 }
 
